1d48ede480bcf3c5c348bb37138a0b96ff27844e
michelldenjoy <michelldenjoy@gmail.com>
2025-10-15 18:04:30 +0200

    Fix import for ApartmentContext

--- src/pages/ApartmentsPage.jsx
+++ src/pages/ApartmentsPage.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import HeaderApartment from "../components/Apartaments/HeaderApart";
 import ApartmentsRoutes from "../Routes/ApartmentsRoutes";
-import { ApartmentProvider } from "../Context/ApartmentsContext";
+import { ApartmentProvider } from "../Context/ApartmentContext";
 
 export default function ApartmentsPage() {
   return (
