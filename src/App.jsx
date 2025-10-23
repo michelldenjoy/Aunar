ea17c946641bd068cd6374bc122bebba81040989
michelldenjoy <michelldenjoy@gmail.com>
2025-10-23 16:23:50 +0200

    bubble

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import NotFound from './pages/NotFound';
 import Contact from './pages/Contact';
 import VillasPage from './pages/VillasPage';
 import Footer from './components/Footer';
+import WhatsAppBubble from './components/WhatsappBubble';
 
 
 
@@ -30,6 +31,7 @@ const App = () => {
 
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <WhatsAppBubble />
       <Footer />
     </BrowserRouter>
   );
---
+++ src/components/WhatsappBubble.jsx
@@ -0,0 +1,21 @@
+// src/components/common/WhatsAppBubble.jsx
+import React from "react";
+import { MessageCircleMore } from "lucide-react";
+
+export default function WhatsAppBubble() {
+  const phoneNumber = "34623380416"; // ☎️ 
+  const message = "¡Hola! Estoy interesado en las propiedades de AUNAR."; // Mensaje predeterminado
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-2 right-3 bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg p-4 transition-transform duration-300 hover:scale-110 z-50 flex items-center justify-center"
+      aria-label="Contáctanos por WhatsApp"
+    >
+      <MessageCircleMore className="w-6 h-6" />
+    </a>
+  );
+}
