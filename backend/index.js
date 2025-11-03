cdaea85cc999b39b7b33701d21b93c6166c79197
michelldenjoy <michelldenjoy@gmail.com>
2025-11-03 09:45:07 +0100

    installing back

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://aunar.vercel.app"
+  ],
   methods: ["GET", "POST", "OPTIONS"],
 }));
 
